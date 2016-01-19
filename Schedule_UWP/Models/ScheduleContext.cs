c33a7a29deb9b7771beb847289653f0f13f96c61
Lukin Egor <lukin.net@gmail.com>
2016-01-19 13:43:47 +0300

    init

---
+++ Schedule_UWP/Models/ScheduleContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Entity;
+using Models;
+
+namespace Schedule_UWP.Models
+{
+    public class ScheduleContext : DbContext
+    {
+        public DbSet<Faculty> Faculties { get; set; }
+        public DbSet<Cathedra> Cathedries { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename=Blogging.db");
+        }
+    }
+}
