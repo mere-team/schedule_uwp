a0ca63cb069914ac5d14ba44cd1acf6bebf9355f
Lukin Egor <lukin.net@gmail.com>
2016-01-19 20:34:28 +0300

    add base userControls

---
+++ Schedule_UWP/TeacherPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Schedule_UWP
+{
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class TeacherPage : Page
+    {
+        public TeacherPage()
+        {
+            InitializeComponent();
+
+            toolBar.ButtonToolBar += OpenCloseSideBar;
+        }
+
+        private void OpenCloseSideBar(object sender, EventArgs e)
+        {
+            if (sideBar.Visibility == Visibility.Collapsed)
+                sideBar.Visibility = Visibility.Visible;
+            else
+                sideBar.Visibility = Visibility.Collapsed;
+        }
+    }
+}
---
+++ Schedule_UWP/ToolBarControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Schedule_UWP
+{
+    public sealed partial class ToolBarControl : UserControl
+    {
+        public ToolBarControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public event EventHandler ButtonToolBar = delegate { };
+
+        private void OnButtonToolBar()
+        {
+            ButtonToolBar(this, EventArgs.Empty);
+        }
+
+        private void ToolBarButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            OnButtonToolBar();
+        }
+    }
+}
